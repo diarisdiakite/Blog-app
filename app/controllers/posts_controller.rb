5ad4e5490ed4843f6b5608dd9f6533d5e32e4951
diarisdiakite <diarisdiakite@gmail.com>
2023-10-25 14:49:58 +0200

    Move list_most_recent_posts and find_most_recent_comments for a post methods to the related models.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,2 @@
 class PostsController < ApplicationController
-  # Create a method which returns the 5 most recent comments for a given post.
-  def find_most_recent_comments(post_id)
-    number_of_comments = 5
-    post = Post.find(post_id)
-
-    if post.comments.exists?
-      displayed_comments = comments.order(created_at: :desc).limit(number_of_comments)
-      puts "Most recent comments for post #{post.title}"
-
-      displayed_comments.each do |comment|
-        truncated_text = comment.text[0, 50]
-        comment.text
-
-        if comment.text.length > 50
-          puts "#{truncated_text} ...[Read more], #{comment.created_at} "
-        else
-          puts "#{comment.text} #{comment.created_at}"
-        end
-      end
-    else
-      puts 'There is no recent comment for this post'
-    end
-  end
 end
