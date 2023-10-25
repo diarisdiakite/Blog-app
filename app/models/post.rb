3fa9a18c87940baf0a0a575831d3c09076ad964b
diarisdiakite <diarisdiakite@gmail.com>
2023-10-26 01:42:24 +0200

    Add Post and User models' validations.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :update_posts_counter
 
   # Create method that gets the comments counter for a post.
