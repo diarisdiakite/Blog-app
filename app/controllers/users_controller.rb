09553e7d9fe6971c0befeb07ece4bf2cff22968e
diarisdiakite <diarisdiakite@gmail.com>
2023-10-25 13:33:32 +0200

    Generate controllers. Add the list_most_recent_posts and the find_most_recent_comments for a post methods in the related controllers.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  # Create a method that returns the 3 most recent posts for a given user.
+  def list_most_recent_posts
+    number_of_posts = 3
+    if posts.exists?
+      displayed_posts = posts.order(created_at: :desc).limit(number_of_posts)
+
+      if displayed_posts.any?
+        puts "Most recent posts for #{user.name}:"
+        displayed_posts.each do |post|
+          puts "#{post.title}, #{post.created_at}"
+        end
+      else
+        puts 'No posts found'
+      end
+    else
+      puts 'User does not exist'
+    end
+  end
+end
