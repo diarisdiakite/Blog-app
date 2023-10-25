b81177632198a8890bdee58718bc5c853e52f2d8
diarisdiakite <diarisdiakite@gmail.com>
2023-10-25 13:31:25 +0200

    Add update_posts_counter, update_comments_counter and update_likes_counter methods to the Post and User Models.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  # Create method that updates the comments counter for a post.
+  def update_comments_counter
+    update(comments_counter: comments.count)
+  end
+
+  # Create method that updates the likes counter for a post.
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
+
+  def update_posts_counter
+    update(posts_counter: posts.count)
+  end
 end
