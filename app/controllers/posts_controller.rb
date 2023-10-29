0a703fb18fe4478cb874ed0d89e1499f3b2a42bc
diarisdiakite <diarisdiakite@gmail.com>
2023-10-30 00:53:16 +0100

    Add the posts controller actions.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  before_action :set_user
+  before_action :set_post, only: [:show]
+
+
+  # Create an index action taking the user id as a parameter
+  def index
+    @posts = @user.posts
+  end
+
+  # create a show action
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = @user.posts.find(params[:id])
+  end
 end
