c2ba0b8bf770f5029b7c9e0a14c1f0fdc4da4059
diarisdiakite <diarisdiakite@gmail.com>
2023-11-03 07:19:08 +0100

    Add the before action for current_user. Add the new, create and like actions to the PostsController.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :current_user
   before_action :set_user
   before_action :set_post, only: [:show]
 
@@ -13,18 +14,52 @@ class PostsController < ApplicationController
     end
   end
 
+  def new
+    # @user = User.find(params[:user_id])
+    # @user = current_user
+    @post = Post.new
+
+    respond_to do |format|
+      format.html { render :new, locals: {post: @post, user: @user} }
+    end
+  end
+  
+  def create
+    @post = current_user.posts.build(posts_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
   # create a show action
   def show
     @post = Post.find(params[:id])
   end
 
+  def like
+    @post = Post.find(params[:id])
+    current_user.likes.create(post: @post)
+    redirect_to posts_path(@post)
+  end
+
   private
 
   def set_user
-    @user = User.find(params[:user_id])
+    @user = @current_user
+  end
+
+  def current_user
+    @current_user ||= User.first
   end
 
   def set_post
     @post = @user.posts.find(params[:id])
+  end
+
+  def posts_params
+    params.require(:post).permit(:title, :text)
   end
 end
