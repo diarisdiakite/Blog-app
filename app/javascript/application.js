66ccca2802aad918b5915972bb90fd7aae9146f2
diarisdiakite <diarisdiakite@gmail.com>
2023-11-07 07:55:27 +0100

    Update controllers path in application.js file.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,2 @@
 import "@hotwired/turbo-rails"
-import "controllers"
-
-import { Turbo } from "@hotwired/turbo-rails"
-import { TurboDrive } from "@hotwired/turbo-rails"
-import { TurboFrames } from "@hotwired/turbo-rails"
-import { TurboStream } from "@hotwired/turbo-rails"
-import { TurboStreams } from "@hotwired/turbo-rails"
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import "./controllers"
