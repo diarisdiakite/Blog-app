9139e5d473bf632f2630bdce3346c49aa0c8b5bb
diarisdiakite <diarisdiakite@gmail.com>
2023-11-08 08:42:16 +0100

    Update ApplicationController. Run linters.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 ﻿class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
-  helper_method :current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
-  def current_user
-    @current_user ||= User.first
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :bio, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:name, :bio, :email, :password, :current_password)
+    end
   end
 end
