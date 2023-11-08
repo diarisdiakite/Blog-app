c859f23448de0efbb89ad8f4bcdc3b6f25b481c5
diarisdiakite <diarisdiakite@gmail.com>
2023-11-08 08:02:46 +0100

    Allow confirmable and recoverable. Run migrations to add table columns and uniqueness to email.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_013212) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_08_042954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_013212) do
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "posts"
