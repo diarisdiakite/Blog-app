1c556ea1a2760992f2646018ee51fdb6954c67a2
Alhassan Osman <oalhassan847@gmail.com>
2023-11-08 20:55:38 +0000

    Add comments destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+
+  def destroy
+    if can?(:destroy, @comment)
+      @comment.destroy
+      redirect_to user_post_path(@user, @post), notice: 'Comment was successfully deleted'
+    else
+      redirect_to user_post_path(@user, @post), alert: 'You are not authorized to delete this comment'
+    end
+  end
 end
